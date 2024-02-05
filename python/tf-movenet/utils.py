d378ae70b5d93df2d116a1f52a30d7cb25bbeeab
Noriyuki Fujimura <nori_fujimura@me.com>
2024-02-05 09:14:26 +0900

    update

--- python/tf-movenet/utils.py
+++ python/tf-movenet/utils.py
@@ -97,17 +97,17 @@ def visualize(
     else:
       person_color = keypoint_color
 
-    # Draw all the landmarks
-    for i in range(len(keypoints)):
-      if keypoints[i].score >= keypoint_threshold:
-        cv2.circle(image, keypoints[i].coordinate, 2, person_color, 4)
-
     # Draw all the edges
     for edge_pair, edge_color in KEYPOINT_EDGE_INDS_TO_COLOR.items():
       if (keypoints[edge_pair[0]].score > keypoint_threshold and
           keypoints[edge_pair[1]].score > keypoint_threshold):
         cv2.line(image, keypoints[edge_pair[0]].coordinate,
                  keypoints[edge_pair[1]].coordinate, edge_color, 2)
+
+      # Draw all the landmarks
+    for i in range(len(keypoints)):
+      if keypoints[i].score >= keypoint_threshold:
+        cv2.circle(image, keypoints[i].coordinate, 2, person_color, 4)
 
     # Draw bounding_box with multipose
     if bounding_box is not None:
